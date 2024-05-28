2d4705d1b18b8dfcfc2d03b482b7a7ff98187f13
Paul Méhaud <paul.mehaud@insa-rouen.fr>
2024-05-28 15:03:37 +0200

    Adding x, y coordinates to the extracted dataFrame

--- src/neighbours_delaunay/useful_methods/dataframes.py
+++ src/neighbours_delaunay/useful_methods/dataframes.py
@@ -9,11 +9,11 @@ from copy import deepcopy
 # Extracting data from a dataframe #
 #==================================#
 
-def extract_data(df, provider=None, techno=None, department=None, region=None, min_info=False):
+def extract_data(df, provider=None, techno=None, department=None, region=None, min_info=True):
     res = deepcopy(df)
 
     if(min_info):
-        res = res[['nom_op', 'num_site', 'longitude', 'latitude', 'nom_reg', 'nom_dep', 'site_2g', 'site_3g', 'site_4g', 'site_5g']]
+        res = res[['nom_op', 'num_site', 'x', 'y', 'longitude', 'latitude', 'nom_reg', 'nom_dep', 'site_2g', 'site_3g', 'site_4g', 'site_5g']]
     if(provider):
         res = res.loc[res['nom_op'] == provider]
         res = res.drop(columns=['nom_op'])
