a4c9cefd822eb1d68ba65360774584f2c1cc9b98
Paul Méhaud <paul.mehaud@insa-rouen.fr>
2024-06-05 16:18:25 +0200

    Optimisation of criteria

--- src/useful_methods/neighbours_delaunay/enhanced_criteria.py
+++ src/useful_methods/neighbours_delaunay/enhanced_criteria.py
@@ -28,7 +28,7 @@ def distance_elim(G, pos, edge, max_distance):
 # Criteria #
 #==========#
 
-def distance_criterion_enhanced(G: nx.Graph, pos: dict, distance_range: dict = {'1': 1, '<1->0.6': 7, '<=0.6->0': 10, '0': 15}, **kwargs) -> nx.Graph:
+def distance_criterion_enhanced(G: nx.Graph, pos: dict, distance_range: dict = {'1': 1.3, '<1->0.6': 7, '<=0.6->0': 10, '0': 15}, **kwargs) -> nx.Graph:
     """ Removes all the edges of G wich are longer than the distance_range.
         
         Parameters
@@ -45,21 +45,36 @@ def distance_criterion_enhanced(G: nx.Graph, pos: dict, distance_range: dict = {
         modif_G : Graph
             The modified graph.
     """
-    cityness_proba = kwargs.get('cityness_proba', None)
-    if(cityness_proba is None):
-        cityness_proba = city_detection(pd.DataFrame(data=pos.values(), columns=['lat','long'], index=pos.keys()))['probas']
+    # cityness_proba = kwargs.get('cityness_proba', None)
+    # if(cityness_proba is None):
+    #     cityness_proba = city_detection(pd.DataFrame(data=pos.values(), columns=['lat','long'], index=pos.keys()))['probas']
+
+    mean_distances = kwargs.get('mean_distance_to_NN', None)
+    if(mean_distances is None):
+       mean_distances = mean_distance_to_NN(pd.DataFrame(data=pos.values(), columns=['lat','long'], index=pos.keys()))
     
     modif_G = deepcopy(G)
     
     for node in tqdm(pos.keys(), desc="nodes - distance"):
-        if(cityness_proba[node] == 0):
-            max_distance = distance_range['0']
-        elif(cityness_proba[node] == 1):
-            max_distance = distance_range['1']
-        elif((cityness_proba[node] < 1) and (cityness_proba[node] > 0.6)):
-            max_distance = distance_range['<1->0.6']
-        elif((cityness_proba[node] <= 0.6) and (cityness_proba[node] > 0)):
-            max_distance = distance_range['<=0.6->0']
+        # if(cityness_proba[node] == 0):
+        #     max_distance = distance_range['0']
+        # elif(cityness_proba[node] == 1):
+        #     max_distance = distance_range['1']
+        # elif((cityness_proba[node] < 1) and (cityness_proba[node] > 0.6)):
+        #     max_distance = distance_range['<1->0.6']
+        # elif((cityness_proba[node] <= 0.6) and (cityness_proba[node] > 0)):
+        #     max_distance = distance_range['<=0.6->0']
+        
+        if(mean_distances[node] <= 1.0):
+            max_distance = 2
+        elif((mean_distances[node] > 1) and (mean_distances[node] <= 2)):
+            max_distance = 5
+        elif((mean_distances[node] > 2) and (mean_distances[node] <= 5)):
+            max_distance = 10
+        elif((mean_distances[node] > 5) and (mean_distances[node] <= 10)):
+            max_distance = 15
+        else:
+            max_distance = 15
         
         for edge in G.edges(node):
             if(km_distance(pos[edge[0]],pos[edge[1]]) > max_distance):
@@ -145,10 +160,8 @@ def angle_criterion_enhanced(G: nx.Graph, pos: dict, angle_range: dict = {'1': 4
             min_angle = 25
         elif((mean_distances[node] > 5) and (mean_distances[node] <= 10)):
             min_angle = 15
-        elif((mean_distances[node] > 10) and (mean_distances[node] <= 15)):
-            min_angle = 10
         else:
-            min_angle = 5
+            min_angle = 10
 
         neighbours = [edge[1] for edge in modif_G.edges(node)]
         angles = compute_angles(node, neighbours, pos)
