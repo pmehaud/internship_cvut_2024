65cf5fa82a1aa4261b68d0be70d0adc5af0ae498
Paul Méhaud <paul.mehaud@insa-rouen.fr>
2024-07-22 14:17:25 +0200

    Implementation of gabriel graph creation

--- src/python_scripts/graphs/graphs_creation.py
+++ src/python_scripts/graphs/graphs_creation.py
@@ -3,14 +3,17 @@
 #=======================#
 
 import networkx as nx # type: ignore
+from pandas import DataFrame
 from scipy.spatial import Delaunay # type: ignore
 from itertools import combinations
+from sklearn.neighbors import NearestNeighbors
+from numpy import sqrt, sum
 
 #=======================================================#
 # Creation of a graph based on a delaunay triangulation #
 #=======================================================#
 
-def delaunay_graph(df):
+def delaunay_graph(df: DataFrame) -> tuple[nx.Graph, dict]:
     """ Returns a Networkx Graph based on the Delaunay triangulation and the position of each node.
         
         Parameters
@@ -39,5 +42,42 @@ def delaunay_graph(df):
     G = nx.relabel_nodes(G, dict(zip(nodes, df.index))) # renaming nodes according to the indexes of the dataframe
 
     pos = dict(zip(df.index, points_pos)) # gives each node his own position
+
+    return G, pos
+
+#=================================================================#
+# Creation of the gabriel graph based on a delaunay triangulation #
+#=================================================================#
+
+def gabriel_graph(df: DataFrame) -> tuple[nx.Graph, dict]:
+    """ Returns a Gabriel Graph based on the Delaunay triangulation and the position of each node.
+        
+        Parameters
+        ----------
+        df : DataFrame
+            The pandas DataFrame of your data.
+
+        Returns
+        -------
+        G : Graph
+            A Gabriel Graph.
+        pos : dict
+            The position of G's nodes.
+    """
+    G, pos = delaunay_graph(df)
+
+    coordsXY = df[['x','y']]
+
+    for edge in G.edges:
+        pt1 = edge[0]
+        pt2 = edge[1]
+
+        middle_point = (coordsXY.loc[pt1] + coordsXY.loc[pt2])/2
+
+        neigh = NearestNeighbors(radius=sqrt(sum((coordsXY.loc[pt1] - coordsXY.loc[pt2])**2, axis=0))/2)
+        neigh.fit(coordsXY)
+
+        if(len(coordsXY.iloc[neigh.radius_neighbors([middle_point], sort_results=True)[1][0][:-2]].index)>0):
+            G.remove_edges_from([edge])
 
     return G, pos
