a4bae3ee0862bce8673a5dd6c207ea59492fae42
Paul Méhaud <paul.mehaud@insa-rouen.fr>
2024-07-29 11:33:28 +0200

    Addition of new variables for simple criteria and modification of min_angle

--- src/global_variables.py
+++ src/global_variables.py
@@ -1,10 +1,14 @@
 # Parameters for criteria using 3NN method (angle in degrees and distance in kilometer)
 MEAN_DISTANCE_PARAMS = {
     ']0, 1] km': {'colour': '#030464', 'min_angle': 5, 'max_distance': 2},
-    ']1, 2] km': {'colour': '#069AF3', 'min_angle': 10, 'max_distance': 5},
-    ']2, 4] km': {'colour': '#02D4BB', 'min_angle': 15, 'max_distance': 10},
-    ']4, inf] km': {'colour': '#0DBF75', 'min_angle': 20, 'max_distance': 15},
+    ']1, 2] km': {'colour': '#069AF3', 'min_angle': 15, 'max_distance': 5},
+    ']2, 4] km': {'colour': '#02D4BB', 'min_angle': 25, 'max_distance': 10},
+    ']4, inf] km': {'colour': '#0DBF75', 'min_angle': 30, 'max_distance': 15},
 }
+
+# Parameters for simple criteria
+MAX_DISTANCE = 15
+MIN_ANGLE = 20
 
 # Number of neighbours in the kNN city detection method
 N_NEIGH = 3
