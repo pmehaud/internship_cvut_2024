291c7ba1af1937b6d636b938a43798521abd31bb
Paul Méhaud <paul.mehaud@insa-rouen.fr>
2024-08-09 10:11:12 +0200

    Modification of angle criterion values

--- src/global_variables.py
+++ src/global_variables.py
@@ -1,9 +1,9 @@
 # Parameters for criteria using 3NN method (angle in degrees and distance in kilometer)
 MEAN_DISTANCE_PARAMS = {
     ']0, 1] km': {'colour': '#030464', 'min_angle': 5, 'max_distance': 2},
-    ']1, 2] km': {'colour': '#069AF3', 'min_angle': 15, 'max_distance': 5},
-    ']2, 4] km': {'colour': '#02D4BB', 'min_angle': 25, 'max_distance': 10},
-    ']4, inf] km': {'colour': '#0DBF75', 'min_angle': 30, 'max_distance': 15},
+    ']1, 2] km': {'colour': '#069AF3', 'min_angle': 10, 'max_distance': 5},
+    ']2, 4] km': {'colour': '#02D4BB', 'min_angle': 15, 'max_distance': 10},
+    ']4, inf] km': {'colour': '#0DBF75', 'min_angle': 20, 'max_distance': 15},
 }
 
 # Parameters for simple criteria
